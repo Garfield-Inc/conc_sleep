66bb4da6bbb859136b14c8d577b401d23fd953d4
Soumya Satish <88482100+soumya-embold@users.noreply.github.com>
2022-01-04 10:31:49 +0530

    Add files via upload

--- conc_sleep.cpp
+++ conc_sleep.cpp
@@ -1,7 +1,7 @@
-1  #include <pthread.h>
-2  
-3  void foo(pthread_mutex_t *mutex) {
-4    pthread_mutex_lock(mutex);
-5    sleep(30000);
-6    pthread_mutex_unlock(mutex);
-7  }
+#include <pthread.h>
+
+void foo(pthread_mutex_t *mutex) {
+  pthread_mutex_lock(mutex);
+  sleep(30000);
+  pthread_mutex_unlock(mutex);
+}
